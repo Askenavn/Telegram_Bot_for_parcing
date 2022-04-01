cae9a32045ab6c1250352f31e7ff87c34406cfab
Nikita <99843252+Askenavn@users.noreply.github.com>
2022-04-02 00:15:53 +0700

    Update script.py

--- script.py
+++ script.py
@@ -4,17 +4,16 @@ import csv
 import telebot
 import time
 
-token = "5087065097:AAHPxP5l6FD99eObncKMpWNcPlp4RXjGMLE" # Я изучил много информации, но так и не нашел как сделать бота без токена
-channel_id = "@fsdgfsdg" # логин канала куда постятся новости
+token = " " # Я изучил много информации, но так и не нашел как сделать бота без токена
+channel_id = " " # логин канала куда постятся новости
 bot = telebot.TeleBot(token)
 
 
 @bot.message_handler(content_types=['text']) #Bot
 
 def commands(message):
-    #bot.send_message(channel_id, message.text)
+    
     if message.text == "Старт":
-        #bot.send_message(channel_id, "Hello")
         back_item_id = None
         while True:
             post_text = parser(back_item_id)
@@ -22,9 +21,9 @@ def commands(message):
 
             if post_text[0] != None:
                 bot.send_message(channel_id, post_text[0])
-                time.sleep(120)
+                time.sleep(600)
     else:
-        bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши Старт")
+        bot.send_message(message.from_user.id, "Напиши Старт")
 
 #Parcing
 def parser(back_item_id):
