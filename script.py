158a1e02155377472ac8639ca139be856e037429
Nikita <99843252+Askenavn@users.noreply.github.com>
2022-04-01 20:05:12 +0700

    Update script.py

--- script.py
+++ script.py
@@ -4,46 +4,46 @@ import csv
 import telebot
 import time
 
-token = "" # token of your bot
-channel_id = "" # login of channel for posting
+token = "5087065097:AAHPxP5l6FD99eObncKMpWNcPlp4RXjGMLE" # Я изучил много информации, но так и не нашел как сделать бота без токена
+channel_id = "@fsdgfsdg" # логин канала куда постятся новости
 bot = telebot.TeleBot(token)
 
 
 @bot.message_handler(content_types=['text']) #Bot
 
 def commands(message):
-    
+    #bot.send_message(channel_id, message.text)
     if message.text == "Старт":
-        
-        back_clock = None
+        #bot.send_message(channel_id, "Hello")
+        back_item_id = None
         while True:
-            post_text = parser(back_clock)
-            back_clock = post_text[1]
+            post_text = parser(back_item_id)
+            back_item_id = post_text[1]
 
             if post_text[0] != None:
                 bot.send_message(channel_id, post_text[0])
-                time.sleep(1800)
+                time.sleep(120)
     else:
         bot.send_message(message.from_user.id, "Я тебя не понимаю. Напиши Старт")
 
 #Parcing
-def parser(back_clock):
-    URL = 'https://vc.ru/new/' #URL of site for parcing
+def parser(back_item_id):
+    URL = 'https://vc.ru/new/' #URL сайта для парсинга
 
     r = requests.get(URL)
     soup = BeautifulSoup(r.content, "html.parser")
-    item = soup.find('div', class_='feed__item') 
-    clock = item.find('time', class_='time', title=True)
-    clock = clock["title"]
+    item = soup.find('div', class_='feed__item') #находим последнюю опубликованную новость
+    item_id = item.find('div', class_='l-mb-28')
+    item_id = item_id["class"]
 
 
-    if clock != back_clock:
-        link = item.find('a', class_='content-link', href=True)["href"].strip()
-        title = item.find('div', class_='content-title content-title--short l-island-a').text.strip()
+    if item_id != back_item_id:
+        link = item.find('a', class_='content-link', href=True)["href"].strip()  # находим ссылку на нее
+        title = item.find('div', class_='content-title content-title--short l-island-a').text.strip()  # находим название
         description = item.find('p').text.strip()
 
-        return f"{title}\n\n{description}\n\n{link}", clock
+        return f"{title}\n\n{description}\n\n{link}", item_id
     else:
-        return None, clock
+        return None, item_id
 
 bot.polling()
